3773f76c5c6b57f23c3dda8dcd131e7ebd3e36d2
StanLachie <lachiejeffress@gmail.com>
2026-03-03 11:15:52 +1000

    Fix CSS isolation, process.env error, and star rendering

--- src/utilities/EmbedPreview.tsx
+++ src/utilities/EmbedPreview.tsx
@@ -16,11 +16,7 @@ export default function EmbedPreview({
     // Fetch data from reddit
     setLoading(true);
     fetch(
-      `https://inbox-api.smartyr.biz/api/v1/review/${
-        import.meta.env.DEV
-          ? "b73206af-bd61-4400-a8ab-3c994f25ab56"
-          : googleCredential.publicReviewAPIKey
-      }`
+      `https://inbox-api.smartyr.biz/api/v1/review/${googleCredential.publicReviewAPIKey}`
     )
       .then((response) => response.json())
       .then((data) => {
